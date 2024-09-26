dcbd1f7a5f58b6fd1a231cdc7ee0fcf7b9d7bb67
FourSqweez <jeerasak.kr@chomchob.com>
2024-09-26 15:54:07 +0700

    Add : create mock user for axios

---
+++ src/components/UserList.test.jsx
@@ -0,0 +1,24 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import axios from "axios";
+import UserList from "./UserList";
+
+// Mock axios
+vi.mock("axios");
+
+describe("Test UserList component", () => {
+  const mockUsers = [
+    {
+      id: "1",
+      name: "John Doe",
+      email: "john@example.com",
+      phoneNumber: "1234567890",
+    },
+    {
+      id: "2",
+      name: "Jane Doe",
+      email: "jane@example.com",
+      phoneNumber: "0987654321",
+    },
+  ];
+});

0ceada0e9087b0f7fe4849b347086e71298aa24d
FourSqweez <jeerasak.kr@chomchob.com>
2024-09-26 15:48:56 +0700

    Add : create userList component to get data from mock service

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import "./App.css";
 import Counter from "./components/Counter";
+import UserList from "./components/UserList";
 
 function App() {
   return (
-    <>
-      <div>Test</div>
-      <Counter />
-    </>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        gap: "24px",
+        margin: "24px",
+        overflow: "hidden",
+      }}>
+      <div>
+        <h1>Test Counter</h1>
+        <Counter />
+      </div>
+      <div>
+        <h1>Test User list</h1>
+        <UserList />
+      </div>
+    </div>
   );
 }
 
